16fc71180cd61d4f6f19a291511894ac4ed175f3
Mustafa Hakan Kara <mustafahakankara@hotmail.com>
2022-02-06 20:50:31 +0300

    Answer editing minor bug fix.

--- Controllers/AnswersController.cs
+++ Controllers/AnswersController.cs
@@ -66,7 +66,7 @@ namespace OutOfBound
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Text,Rating")] AnswerModel answerModel)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Text,QuestionModelID")] AnswerModel answerModel)
         {
             if (id != answerModel.ID)
             {
